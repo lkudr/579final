10aa055c4c65184f6146d3a617995e1050bafa26
Luke Kudryashov <55161526+lkudr@users.noreply.github.com>
2021-04-19 16:01:04 -0400

    Add files via upload

--- js/distraction.js
+++ js/distraction.js
@@ -13,7 +13,7 @@ if (puppies_selector.checked) {
     category = "kittens";
 }
 
-fetch('http://api.giphy.com/v1/gifs/random?tag='+category+'&api_key=JMPx7E7lxgpLIC2gUx6Wd7L40VnUHJIj&rating=g').then(response => response.json())
+fetch('https://api.giphy.com/v1/gifs/random?tag='+category+'&api_key=JMPx7E7lxgpLIC2gUx6Wd7L40VnUHJIj&rating=g').then(response => response.json())
 .then((result) => {
     console.log(result["data"]["images"]["downsized"]["url"]);
     let new_image = document.createElement('img');
